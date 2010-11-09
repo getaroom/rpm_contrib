8d03092efac621060c2cab9f3b5b94213eb6c9ac
Ben Poweski <bpoweski@gmail.com>
2010-11-09 16:13:00 -0600

    remove unused mongo cursor instrumentation

--- lib/rpm_contrib/instrumentation/mongo.rb
+++ lib/rpm_contrib/instrumentation/mongo.rb
@@ -10,17 +10,6 @@ if defined?(::Mongo) && !NewRelic::Control.instance['disable_mongodb']
         end
       end
     end
-
-    class Cursor
-      class << self
-        alias :old_included :included
-
-        def included(model)
-          old_included(model)
-          super
-        end
-      end
-    end
   end
 
   module RPMContrib::Instrumentation
@@ -32,20 +21,7 @@ if defined?(::Mongo) && !NewRelic::Control.instance['disable_mongodb']
               :find, :find_one, :save, :insert, :remove, :update, :create_index, :drop_index, :drop,
               :find_and_modify, :map_reduce, :group, :distinct, :rename, :index_information, :options, :stats, :count
             ].each do |method|
-              add_method_tracer method, "Database/#{self.class.name}/#{method}"
-            end
-          end
-          super
-        end
-      end
-
-      module Cursor
-        def included(model)
-          model.class_eval do
-            [
-              :next_document
-            ].each do |method|
-              add_method_tracer method, "Database/Mongo/#{method}"
+              add_method_tracer method, ['Database/#{self.class.name}/', method.to_s].join
             end
           end
           super
@@ -53,6 +29,5 @@ if defined?(::Mongo) && !NewRelic::Control.instance['disable_mongodb']
       end
     end
     ::Mongo::Collection.extend(RPMContrib::Instrumentation::Mongo::Collection)
-    ::Mongo::Cursor.extend(RPMContrib::Instrumentation::Mongo::Cursor)
   end
 end
