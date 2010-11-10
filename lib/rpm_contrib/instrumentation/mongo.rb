02b1bd975f1d16c5663a98559de6ba78245537a8
Ben Poweski <bpoweski@gmail.com>
2010-11-10 10:33:48 -0600

    change instrumentation to use class_eval for Mongo classes

--- lib/rpm_contrib/instrumentation/mongo.rb
+++ lib/rpm_contrib/instrumentation/mongo.rb
@@ -1,33 +1,18 @@
 if defined?(::Mongo) && !NewRelic::Control.instance['disable_mongodb']
-  module Mongo #:nodoc:
-    class Collection
-      class << self
-        alias :old_included :included
-
-        def included(model)
-          old_included(model)
-          super
-        end
-      end
+  Mongo::Collection.class_eval do
+    [
+      :find, :find_one, :save, :insert, :remove, :update, :create_index, :drop_index, :drop,
+      :find_and_modify, :map_reduce, :group, :distinct, :rename, :index_information, :options, :stats, :count
+    ].each do |method|
+      add_method_tracer method, ['Database/#{self.class.name}/', method.to_s].join
     end
   end
 
-  module RPMContrib::Instrumentation
-    module Mongo
-      module Collection
-        def included(model)
-          model.class_eval do
-            [
-              :find, :find_one, :save, :insert, :remove, :update, :create_index, :drop_index, :drop,
-              :find_and_modify, :map_reduce, :group, :distinct, :rename, :index_information, :options, :stats, :count
-            ].each do |method|
-              add_method_tracer method, ['Database/#{self.class.name}/', method.to_s].join
-            end
-          end
-          super
-        end
-      end
+  ::Mongo::Cursor.class_eval do
+    [
+      :next_document, :count, :refresh, :to_a
+    ].each do |method|
+      add_method_tracer method, ['Database/#{self.class.name}', method.to_s].join
     end
-    ::Mongo::Collection.extend(RPMContrib::Instrumentation::Mongo::Collection)
   end
 end
